a149f13b74bf1a79d1f05a3c21151ea94aacb785
rajasri9949 <rajasrichalamala@gmail.com>
2024-10-24 18:45:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://55vu1vugpb.execute-api.us-west-2.amazonaws.com/DEV' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
